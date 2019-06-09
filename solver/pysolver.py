18142c3a95fc7bbe49e06aa68bc35d0dea10136d
Vaibhav Ramamoorthy <vaibhavr@berkeley.edu>
2019-06-09 13:53:48 -0700

    finished

--- solver/pysolver.py
+++ solver/pysolver.py
@@ -1,52 +1,68 @@
 import numpy as np
-from setup import Grid, Piece
+from iqutils import Grid, Piece
 from timeit import default_timer as timer
 
-def get_solutions(pieces, sol_path, log_path):
+def get_solutions(pieces, sol_path, log_path, starting_grid = Grid()):
     sol_file = open(sol_path, "w")
     log_file = open(log_path, "w")
-    sol_num = 0
-    
+
     SOLUTIONS = []
-    
     def recurse(grid, piece_to_add, x, y, orientation, other_remaining_pieces, k):
         # print("Adding " + piece_to_add.color + " to " + str((x, y)))
         # grid.cprint()
+        # if piece_to_add.color == "lg":
+        # 	grid.cprint()
         new_piece = piece_to_add.reorient(piece_to_add.orientations[orientation])
         xdim = new_piece.grid.shape[0]
         ydim = new_piece.grid.shape[1]
         if x + xdim <= 8 and y + ydim <= 8:
             grid.add_piece(piece_to_add, x, y, orientation)
+            # if piece_to_add.color == "br":
+            #     print()
+            #     grid.cprint()
+            #     print()
+            # if piece_to_add.color == "pu":
+            #     print()
+            #     grid.cprint()
+            #     print()
             log_file.write(">"*k + " " + piece_to_add.color + " " + str((x, y, orientation)) + "\n")
             if grid.good_grid() and not other_remaining_pieces:
+                sol_num = len(SOLUTIONS)
                 sol_file.write("Solution " + str(sol_num) + ":\n")
                 sol_file.write(grid.grid_to_string() + "\n\n")
+                log_file.write("! solution found\n")
                 print("Solution " + str(sol_num))
-                grid.cprint()
+                # grid.cprint()
                 SOLUTIONS.append(grid)
-                sol_num += 1
             elif grid.good_grid():
                 next_piece = other_remaining_pieces[0]
                 for r in range(8):
                     for c in range(8):
-                        if grid.empty_space(r, c):
-                            for o in range(next_piece.max_orient):
-                                new_grid = Grid(grid.grid, grid.colorgrid)
-                                recurse(new_grid, next_piece, r, c, o, other_remaining_pieces[1:], k + 1)
+                        for o in range(next_piece.max_orient):
+                            # if next_piece.color == "pu":
+                            #     print(r, c, o)
+                            recurse(Grid(grid.grid, grid.colorgrid), next_piece, \
+                            r, c, o, other_remaining_pieces[1:], k + 1)
     
+    # NOTE: only go until row 3 cause anything after that is just a rotated
+    # version of a previous solution (this is unique to the blue piece
+    # which I am starting with)
     for r in range(8):
         for c in range(8):
-        	# NOTE: don't have to do multiple orientations of first piece since
-        	# we want unique solutions and all solutions with 
-        	# the first piece in any orientation other than 0 will simply be rotations
-        	# or reflections of a solution with the first piece in orientation 0
-        	message = "Piece 1 in row " + str(r) + ", column " + str(c)
-        	log_file.write(message + "\n")
-        	print(message)
-        	start = timer()
-        	recurse(Grid(), pieces[0], r, c, 0, pieces[1:], 1) # see note above
-        	end = timer()
-        	print(">>> Process completed in " + str((end - start)/3600) + " hours")
+            for o in range(pieces[0].max_orient):
+                # NOTE: don't have to do multiple orientations of first piece since
+                # we want unique solutions and all solutions with 
+                # the first piece in any orientation other than 0 will simply be rotations
+                # or reflections of a solution with the first piece in orientation 0
+                message = pieces[0].color + " in row " + str(r) + ", column " + str(c) + ", orientation " + str(o)
+                # message = "Piece 1 in row " + str(r) + ", column " + str(c)
+                log_file.write(message + "\n")
+                print(message)
+                start = timer()
+                # recurse(starting_grid, pieces[0], r, c, 0, pieces[1:], 1) # see note above
+                recurse(Grid(starting_grid.grid, starting_grid.colorgrid), pieces[0], r, c, o, pieces[1:], 1)
+                end = timer()
+                print(">>> Process completed in " + str((end - start)/3600) + " hours")
 
     sol_file.close()
     log_file.close()
@@ -64,7 +80,25 @@ LIGHTGREEN = Piece([[1,1],[1,0],[1,0]],"lg")
 WHITE = Piece([[1,1],[1,1],[1,0],[1,0],[1,0]], "wh")
 AQUA = Piece([[1,1],[1,1],[1,0]], "aq")
 
-PIECES = [BLUE, BROWN, PURPLE, WHITE, DARKGREEN, YELLOW, ORANGE, PINK, LIGHTGREEN, AQUA]
+PIECES = [BLUE, BROWN, PURPLE, WHITE, DARKGREEN, YELLOW, ORANGE, PINK, AQUA, LIGHTGREEN]
 
-solutions = get_solutions(PIECES, "solutions.txt", "log.txt")
+## 00x, 01x, 02x, 10x, 11x, 12x, 13x, 20x, 21, 22, 23
+
+blue_in_21 = Grid()
+blue_in_21.add_piece(BLUE, 2, 1, 0)
+
+solutions21 = get_solutions(PIECES[1:], "solutions_blue_in_2_1.txt", "log_blue_in_2_1.txt", blue_in_21)
+
+blue_in_22 = Grid()
+blue_in_22.add_piece(BLUE, 2, 2, 0)
+
+solutions22 = get_solutions(PIECES[1:], "solutions_blue_in_2_2.txt", "log_blue_in_2_2.txt", blue_in_22)
+
+blue_in_23 = Grid()
+blue_in_23.add_piece(BLUE, 2, 3, 0)
+
+solutions23 = get_solutions(PIECES[1:], "solutions_blue_in_2_3.txt", "log_blue_in_2_3.txt", blue_in_23)
+
+
+# solutions = get_solutions(PIECES, "solutions.txt", "log.txt")
 
